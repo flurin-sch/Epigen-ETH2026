0f1a3ce70377c1bdcdefc003943541331fe36e50
flurin-sch <85993825+flurin-sch@users.noreply.github.com>
2026-02-20 16:45:35 +0100

    Complete assignment for week 01 to test R installation

--- week01/install.R
+++ week01/install.R
@@ -1,12 +1,12 @@
 install.packages(c("gsl","ggplot2","remotes","rmarkdown"))
-# If you haven't install Bioconductor yet:
-# install.packages("BiocManager")
-# BiocManager::install()
+# If you haven't installed Bioconductor yet:
+install.packages("BiocManager")
+BiocManager::install()
 
 BiocManager::install(c("GenomicRanges", "rtracklayer", "EnrichedHeatmap", "AnnotationHub", 
                        "ensembldb", "edgeR", "esATAC", "sechm", "motifmatchr","rGREAT",
                        "bsseq","DMRcate","data.table","InteractionSet","limma",
                        "universalmotif", "MotifDb", "TFBSTools", "Biostrings", "PWMEnrich",
-                       "Rsubread","Rfastp"))
-BiocManager::install(c("Bioconductor/BiocFileCache", "chromVAR"))
-BiocManager::install("ETHZ-INS/epiwraps", ref="devel")
+                       "Rsubread","Rfastp"), force=TRUE)
+BiocManager::install(c("Bioconductor/BiocFileCache", "chromVAR"), force=TRUE)
+BiocManager::install("ETHZ-INS/epiwraps", ref="devel", force=TRUE)
